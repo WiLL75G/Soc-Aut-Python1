c77dbc50289810569695b15fcb99a767d7410e67
WiLL75G <wgokahp@gmail.com>
2026-01-05 22:42:26 -0800

    Update Day 1 Python exercises

---
+++ Day1/day1.python.py
@@ -0,0 +1,49 @@
+# -------------------------------------------
+# DAY 1 — PRINTING TEXT (YOUR FIRST PYTHON)
+# -------------------------------------------
+
+print("Hello, Python")
+print("I am learning from zero")
+
+
+# OUTPUT :
+# Hello, Python
+# I am learning from zero
+
+
+# EXPLANATION :
+# - print() shows text on the screen
+# - Text must be inside quotes
+# - This is the first thing every beginner learns
+
+
+
+# -------------------------------------------
+
+## TRYING ANOTHER EXAMPLE
+
+print("My name is William")
+print("I will master Python")
+
+
+# OUTPUT :
+# My name is William
+# I will master Python
+
+
+
+# -------------------------------------------
+
+## WHAT I LEARNED TODAY ✅
+# ✔ print()
+# ✔ quotes " "
+# ✔ comments #
+
+# -------------------------------------------
+
+## FINAL DAY 1 PRACTICE 🧠
+
+# My first Python practice
+print("My name is William")
+print(22)
+print("I am learning Python")
